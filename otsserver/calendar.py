ca4eb20f8163bb751caf1400f763c6eb925bbd9e
Peter Todd <pete@petertodd.org>
2012-10-09 00:33:39 -0400

    Add MultiNotaryCalendar

--- otsserver/calendar.py
+++ otsserver/calendar.py
@@ -9,7 +9,13 @@
 # modified, propagated, or distributed except according to the terms contained
 # in the LICENSE file.
 
+import time
+
 from opentimestamps.dag import *
+
+class CalendarError(StandardError):
+    pass
+
 
 class Calendar(object):
     """Link and timestamp collections of digests
@@ -61,3 +67,90 @@ class LinearCalendar(Calendar):
 
         """
         raise NotImplementedError
+
+
+
+class MultiNotaryCalendar(Calendar):
+    """Efficiently sign digests with multiple notaries
+
+    See doc/design.md
+    """
+
+    all_submitted_ops = None
+    known_notaries = None
+    dag = None
+
+    class __NotaryEntry:
+        def __init__(self,**kwargs):
+            self.last_digest_signed_idx = 0
+            self.pending_signatures = {}
+            self.__dict__.update(kwargs)
+
+    class __PendingSignature:
+        def __init__(self,**kwargs):
+            self.created = time.time()
+            self.__dict__.update(kwargs)
+
+    def __init__(self,hash_algorithm='sha256',dag=None):
+        assert(dag is not None)
+        self.dag = dag
+        self.hash_algorithm = hash_algorithm
+        self.all_submitted_ops = []
+        self.known_notaries = {}
+
+    def get_merkle_child(self,notary):
+        if not notary.canonicalized():
+            raise CalendarError("get_merkle_child() expects a notary specification in canonical form")
+
+        digests_to_sign = None
+
+        try:
+            notary_entry = self.known_notaries[notary]
+        except KeyError:
+            notary_entry = self.__NotaryEntry()
+            self.known_notaries[notary] = notary_entry
+
+        start_idx = notary_entry.last_digest_signed_idx
+        end_idx = len(self.all_submitted_ops)
+
+        digests_to_sign = self.all_submitted_ops[start_idx:end_idx]
+
+        tree = build_merkle_tree(digests_to_sign,algorithm=self.hash_algorithm)
+
+        merkle_child = tree[-1]
+
+        notary_entry.pending_signatures[merkle_child.digest] = \
+                self.__PendingSignature(last_digest_signed_idx=end_idx,
+                                        tree=tree)
+
+        return merkle_child
+
+
+    def add_verification(self,verify_op):
+        """Adds a Verify operation"""
+        digest = verify_op.inputs[0]
+        signature = verify_op.signature
+        try:
+            notary_entry = self.known_notaries[signature.notary]
+        except KeyError:
+            raise CalendarError("Notary unknown to us")
+
+        try:
+            pending_signature = notary_entry.pending_signatures[digest]
+        except KeyError:
+            raise CalendarError("Pending signature not found; notary probably has not called get_merkle_child() yet")
+
+        self.dag.update(pending_signature.tree)
+        self.dag.add(verify_op)
+
+        notary_entry.last_digest_signed_idx = pending_signature.last_digest_signed_idx
+
+        notary_entry.pending_signatures.pop(digest)
+
+        # The verification itself needs to become part of the digests verified.
+        self.submit(verify_op)
+
+
+    def submit(self,op):
+        self.all_submitted_ops.append(op)
+        return None
--- otsserver/tests/test_calendar.py
+++ otsserver/tests/test_calendar.py
@@ -12,13 +12,13 @@
 import unittest
 
 from opentimestamps.dag import *
-from opentimestamps.serialization import *
+from opentimestamps.notary import *
 
 from ..calendar import *
 
 class TestLinearCalendar(unittest.TestCase):
     def test_link_inputs_outputs(self):
-        dag = MemoryDag()
+        dag = Dag()
         cal = LinearCalendar(dag=dag)
 
         d1 = Digest('1')
@@ -38,3 +38,53 @@ class TestLinearCalendar(unittest.TestCase):
         h3 = cal.submit((d3,))[0]
         self.assertIn(h3,dag)
         self.assertIn(h3,dag.dependents[h2])
+
+
+
+class TestMultiNotaryCalendar(unittest.TestCase):
+    def test_signing(self):
+        cal = MultiNotaryCalendar(dag=Dag())
+
+        notary1 = TestNotary(identity='pass-1')
+        notary2 = TestNotary(identity='pass-2')
+        notary3 = TestNotary(identity='pass-3')
+
+        digests = []
+        def add_digests(n,accumulator=[0]):
+            for i in range(0,n):
+                accumulator[0] += 1
+                h = Hash(inputs=(bytes(str(accumulator[0])),))
+                digests.append(h)
+                cal.submit(h)
+
+        def check_for_path(source,dest):
+            alleged_path = cal.dag.path(source,dest)
+
+            # Check if another dag thinks the path is ok
+            dag2 = Dag(alleged_path)
+            path2 = dag2.path(source,dest)
+            self.assertTrue(path2 is not None)
+
+        def sign_and_check_all_paths(notary,stamp_time=[0]):
+            stamp_time[0] += 1
+
+            merkle_child = cal.get_merkle_child(notary)
+            sig = notary.sign(merkle_child,stamp_time[0])
+            verify_op = Verify(inputs=(merkle_child,),signature=sig)
+            cal.add_verification(verify_op)
+
+            # There should be a path from every single digest in the dag to our
+            # new verification.
+            for d in cal.dag:
+                check_for_path(d,verify_op)
+
+        add_digests(10)
+        sign_and_check_all_paths(notary1)
+        sign_and_check_all_paths(notary1)
+        add_digests(9)
+        sign_and_check_all_paths(notary2)
+        sign_and_check_all_paths(notary2)
+        add_digests(10)
+        sign_and_check_all_paths(notary3)
+        add_digests(11)
+        sign_and_check_all_paths(notary1)

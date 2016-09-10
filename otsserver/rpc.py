70d935bf301cc2bd2901f7b8ee8dbb1a7aacf343
Peter Todd <pete@petertodd.org>
2016-09-10 12:45:39 -0400

    Use correct Content-type's for replies

--- otsserver/rpc.py
+++ otsserver/rpc.py
@@ -43,7 +43,7 @@ class RPCRequestHandler(http.server.BaseHTTPRequestHandler):
         timestamp = self.aggregator.submit(digest)
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')  # FIXME
+        self.send_header('Content-type', 'application/octet-stream')
         self.end_headers()
 
         ctx = StreamSerializationContext(self.wfile)
@@ -56,7 +56,7 @@ class RPCRequestHandler(http.server.BaseHTTPRequestHandler):
             commitment = binascii.unhexlify(commitment)
         except binascii.Error:
             self.send_response(400)
-            self.send_header('Content-type', 'text/plain')  # FIXME
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'commitment must be hex-encoded bytes')
             return
@@ -65,13 +65,13 @@ class RPCRequestHandler(http.server.BaseHTTPRequestHandler):
             timestamp = self.calendar[commitment]
         except KeyError:
             self.send_response(404)
-            self.send_header('Content-type', 'text/plain')  # FIXME
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'not found')
             return
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')  # FIXME
+        self.send_header('Content-type', 'application/octet-stream')
         self.end_headers()
 
         timestamp.serialize(StreamSerializationContext(self.wfile))

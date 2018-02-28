0c0b0765755b08e0aad7e1b4e7c56ffe4dcb254a
Riccardo Casatta <riccardo@casatta.it>
2018-02-28 12:31:24 +0100

    Including performance changes inside the opentimestamps-server insted of in the library

--- otsserver/stamper.py
+++ otsserver/stamper.py
@@ -22,18 +22,94 @@ import bitcoin.rpc
 from bitcoin.core import COIN, b2lx, b2x, CTxIn, CTxOut, CTransaction, str_money_value
 from bitcoin.core.script import CScript, OP_RETURN, OP_CHECKSIG
 
-from opentimestamps.bitcoin import make_timestamp_from_block
-from opentimestamps.core.notary import PendingAttestation
+from opentimestamps.bitcoin import cat_sha256d
+from opentimestamps.core.notary import PendingAttestation, BitcoinBlockHeaderAttestation
 from opentimestamps.core.serialize import StreamSerializationContext, StreamDeserializationContext
 from opentimestamps.core.op import OpPrepend, OpAppend, OpSHA256
 from opentimestamps.core.timestamp import Timestamp, make_merkle_tree
 from opentimestamps.timestamp import nonce_timestamp
+
 
 from otsserver.calendar import Journal
 
 KnownBlock = collections.namedtuple('KnownBlock', ['height', 'hash'])
 TimestampTx = collections.namedtuple('TimestampTx', ['tx', 'tip_timestamp', 'commitment_timestamps'])
 UnconfirmedTimestampTx = collections.namedtuple('TimestampTx', ['tx', 'tip_timestamp', 'n'])
+
+
+def make_btc_block_merkle_tree(blk_txids):
+    assert len(blk_txids) > 0
+
+    digests = blk_txids
+    while len(digests) > 1:
+        # The famously broken Satoshi algorithm: if the # of digests at this
+        # level is odd, double the last one.
+        if len(digests) % 2:
+            digests.append(digests[-1].msg)
+
+        next_level = []
+        for i in range(0,len(digests), 2):
+            next_level.append(cat_sha256d(digests[i], digests[i + 1]))
+
+        digests = next_level
+
+    return digests[0]
+
+
+def make_timestamp_from_block(digest, block, blockheight, serde_txs, *, max_tx_size=1000):
+    """Make a timestamp for a message in a block with cached serialized txs
+    see python-opentimestamps.bitcoin.make_timestamp_from_block
+    """
+    len_smallest_tx_found = max_tx_size + 1
+    commitment_tx = None
+    prefix = None
+    suffix = None
+    for (tx, serialized_tx) in serde_txs:
+
+        if len(serialized_tx) > len_smallest_tx_found:
+            continue
+
+        try:
+            i = serialized_tx.index(digest)
+        except ValueError:
+            continue
+
+        # Found it!
+        commitment_tx = tx
+        prefix = serialized_tx[0:i]
+        suffix = serialized_tx[i + len(digest):]
+
+        len_smallest_tx_found = len(serialized_tx)
+
+    if len_smallest_tx_found > max_tx_size:
+        return None
+
+    digest_timestamp = Timestamp(digest)
+
+    # Add the commitment ops necessary to go from the digest to the txid op
+    prefix_stamp = digest_timestamp.ops.add(OpPrepend(prefix))
+    txid_stamp = cat_sha256d(prefix_stamp, suffix)
+
+    assert commitment_tx.GetTxid() == txid_stamp.msg
+
+    # Create the txid list, with our commitment txid op in the appropriate
+    # place
+    block_txid_stamps = []
+    for tx in block.vtx:
+        if tx.GetTxid() != txid_stamp.msg:
+            block_txid_stamps.append(Timestamp(tx.GetTxid()))
+        else:
+            block_txid_stamps.append(txid_stamp)
+
+    # Build the merkle tree
+    merkleroot_stamp = make_btc_block_merkle_tree(block_txid_stamps)
+    assert merkleroot_stamp.msg == block.hashMerkleRoot
+
+    attestation = BitcoinBlockHeaderAttestation(blockheight)
+    merkleroot_stamp.attestations.add(attestation)
+
+    return digest_timestamp
+
 
 class OrderedSet(collections.OrderedDict):
     def add(self, item):
@@ -206,6 +282,8 @@ class Stamper:
     def __do_bitcoin(self):
         """Do Bitcoin-related maintenance"""
 
+
+
         # FIXME: we shouldn't have to create a new proxy each time, but with
         # current python-bitcoinlib and the RPC implementation it seems that
         # the proxy connection can timeout w/o recovering properly.
@@ -250,9 +328,9 @@ class Stamper:
 
             # Check all potential pending txs against this block.
             # iterating in reverse order to prioritize most recent digest which commits to a bigger merkle tree
-            for (i, unconfirmed_tx) in enumerate(self.unconfirmed_txs[::-1]):
+            for unconfirmed_tx in self.unconfirmed_txs[::-1]:
                 block_timestamp = make_timestamp_from_block(unconfirmed_tx.tip_timestamp.msg, block, block_height,
-                                                            serde_txs=serde_txs)
+                                                            serde_txs)
 
                 if block_timestamp is None:
                     continue

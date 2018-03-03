223a02dc0423c25eb0f8e40a9289a7935c5bc39b
Riccardo Casatta <riccardo.casatta@gmail.com>
2018-03-03 11:22:11 +0100

    serialize and deserialize of kv_map

--- backup.py
+++ backup.py
@@ -2,33 +2,73 @@
 import otsserver
 from otsserver.calendar import Journal
 from bitcoin.core import b2x
-from opentimestamps.core.serialize import BytesSerializationContext
+from opentimestamps.core.serialize import BytesSerializationContext, BytesDeserializationContext, TruncationError
 
 
-def print_ts(ts, msg):
+def create_kv_map(ts, msg):
     for op, timestamp in ts.ops.items():
         ctx = BytesSerializationContext()
         op.serialize(ctx)
         # print("key:" + b2x(msg) + " value:" + str(op) + "-" + b2x(ctx.getbytes()))
         kv_map[msg] = ctx.getbytes()
-        print_ts(timestamp, timestamp.msg)
+        create_kv_map(timestamp, timestamp.msg)
+
+
+def kv_map_to_bytes():
+    ctx = BytesSerializationContext()
+    for key, value in kv_map.items():
+        ctx.write_varuint(len(key))
+        ctx.write_bytes(key)
+        ctx.write_varuint(len(value))
+        ctx.write_bytes(value)
+
+    return ctx.getbytes()
+
+
+def bytes_to_kv_map(kv_bytes):
+    ctx = BytesDeserializationContext(kv_bytes)
+    new_kv_map = {}
+
+    while True:
+        try:
+            key_len = ctx.read_varuint()
+            key = ctx.read_bytes(key_len)
+            value_len = ctx.read_varuint()
+            value = ctx.read_bytes(value_len)
+            new_kv_map[key] = value
+        except TruncationError:
+            break
+
+    return new_kv_map
+
 
 kv_map = {}
 calendar_path = '/Users/casatta/calendar_backup/exported'
 journal = Journal(calendar_path + '/journal')
 calendar = otsserver.calendar.Calendar(calendar_path)
-i = 0
-while True:
+start = 5000000
+end = 5000200
+for i in range(start, end):
     try:
         current = journal[i]
         # print(str(i) +":"+b2x(journal[i]))
         current_el = calendar[current]
         # print("\t"+str(current_el))
-        print_ts(current_el, current_el.msg)
+        create_kv_map(current_el, current_el.msg)
     except KeyError:
         break
-    i += 1
     if i % 10000 == 0:
         print(str(i) + ":" + b2x(journal[i]))
+
+kv_bytes = kv_map_to_bytes()
+# print("returning " + b2x(kv_bytes))
+print("len " + str(len(kv_bytes)))
+
+new_map = bytes_to_kv_map(kv_bytes)
+# print(str(new_map))
+
+assert len(kv_map) == len(new_map)
+assert kv_map == new_map
+
 
 
